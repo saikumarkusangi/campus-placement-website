4bcfdbfeda3dafae3d57055a33baf14df1bfab28
saikumarkusangi <saikumarkusangi007@gmail.com>
2023-08-13 16:45:45 +0530

    first commit

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,8 +6,9 @@ import Upcoming from '../components/Upcoming'
 function Home() {
   return (
     <div className='w-full flex flex-col gap-4 md:flex-row md:gap-0 md:flex px-2 pt-1 md:ml-[20%] mt-[27%] md:mt-[6%]'>
-        <Calendar/>
+       <div className='hidden md:flex'> <Calendar/></div>
         <Upcoming/>
+       <div className='md:hidden flex'> <Calendar/></div>
     </div>
   )
 }
